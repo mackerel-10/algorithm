a1b52e7781cdf4832e574249fe6684cacf5c7c07
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-05-12 23:40:33 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 116 ms, Memory: 9316 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.js
@@ -16,21 +16,13 @@ const input = fs.readFileSync('/dev/stdin', 'utf-8').trim().split('\n');
 let heights = input.map(Number).sort((a, b) => a - b);
 const margin = heights.reduce((sum, current) => (sum += current), 0) - 100;
 
-let idx1, idx2;
 for (let i = 0; i < 8; i++) {
   for (let j = i + 1; j < 9; j++) {
     if (heights[i] + heights[j] === margin) {
-      // heights.splice(i, 1);
-      // heights.splice(j - 1, 1);
-      [idx1, idx2] = [i, j];
-
-      break;
+      heights.splice(j, 1);
+      heights.splice(i, 1); // i는 항상 j
+      console.log(heights.join('\n'));
+      return;
     }
   }
 }
-
-for (let i = 0; i < 9; i++) {
-  if (i !== idx1 && i !== idx2) console.log(heights[i]);
-}
-
-// console.log(heights.join('\n'));
