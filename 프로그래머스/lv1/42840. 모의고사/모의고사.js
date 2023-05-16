587de2ded4f0f65cb5f0dbe0524e93fcfe36add1
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-05-16 23:13:46 +0900

    [level 1] Title: 모의고사, Time: 336.56 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,38 @@
+function solution(answers) {
+    
+    
+    let students = [];
+    students.push([1, 2, 3, 4, 5]); // 0
+    students.push([2, 1, 2, 3, 2, 4, 2, 5]); // 1
+    students.push(('3'.repeat(2) + '1'.repeat(2) + '2'.repeat(2) + '4'.repeat(2) + '5'.repeat(2)).split('').map(Number)); // 2
+    let correctCnt = [];
+    
+    for (let studentIndex = 0; studentIndex < students.length; studentIndex++) {
+        let student = students[studentIndex];
+        let patternLength = students[studentIndex].length;
+        let cnt = 0;
+        
+        for (let answerIndex = 0; answerIndex < answers.length; answerIndex++) {
+            let restIndex = answerIndex % patternLength;
+            if (answers[answerIndex] === student[restIndex]) {
+                cnt++;
+            }
+        }
+        correctCnt.push(cnt);
+    }    
+
+    const maxCnt = Math.max(...correctCnt);
+    
+    return correctCnt.reduce((result, cnt, index) => {
+        if (cnt === maxCnt) {
+            result.push(index + 1);
+        }
+        
+        return result;
+    }, []);
+}
+
+// n = 1
+// 1번 n, n+1, n+2, n+3, n+4
+// 2번 (2, 1), (2, 3), (2, 4), (2, 5)
+// 3번 (n+2, n+2), (n, n), (n+1, n+1), (n+3, n+3), (n+4, n+4)
