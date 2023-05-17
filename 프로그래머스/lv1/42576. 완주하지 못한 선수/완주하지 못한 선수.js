35c02fd5b1abbf73920226d901a6495682f295c9
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-05-17 23:53:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 107.62 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,12 +1,12 @@
 function solution(participant, completion) {
     participant.sort((playerA, playerB) => playerA > playerB ? 1 : -1);
     completion.sort((playerA, playerB) => playerA > playerB ? 1 : -1);
-    console.log(participant, completion)
     
     let answer = [];
     while (participant.length > 0 || completion.length > 0) {
         let indexA = participant.length - 1;
         let indexB = completion.length - 1;
+        
         if (participant[indexA] === completion[indexB]) {
             participant.pop();
             completion.pop();
