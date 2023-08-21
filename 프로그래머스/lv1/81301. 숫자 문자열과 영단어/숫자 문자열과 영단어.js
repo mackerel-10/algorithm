94a7d907ecfa69e4bab9450b2cd5297038a336fc
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-08-21 18:57:34 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,37 @@
+function solution(s) {
+    let answer = '';
+    const map = {
+        zero: 0,
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9
+    };
+    
+    let combinedString = '';
+    for (let index = 0; index < s.length; index++) {
+        let value = s[index];
+        
+        if (Number.isInteger(Number(value))) {
+            answer += value;
+        } else {
+            combinedString += value;
+            console.log(combinedString);
+            
+            if (map[combinedString] !== undefined) {
+                
+                answer += map[combinedString];
+                combinedString = '';
+            }
+        }
+        
+        // console.log(answer, combinedString);
+    }
+    
+    return Number(answer);
+}
