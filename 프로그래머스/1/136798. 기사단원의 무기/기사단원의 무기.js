ecd23e9fd5dfcd328503438b78d047e938de46c4
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-10-20 16:39:10 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.74 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -4,15 +4,14 @@ function solution(number, limit, power) {
     
     for (let person = 1; person <= number; person++) {
         powerResult = 0;
-        for (let divisor = 1; divisor < Math.sqrt(person); divisor++) {
-            if (person % divisor === 0) {
+        for (let divisor = 1; divisor <= Math.sqrt(person); divisor++) {
+            if (person === Math.pow(divisor, 2)) {
                 powerResult++;
+            } else if (person % divisor === 0) {
+                powerResult += 2;
             }
         }
-        powerResult *= 2;
-        if (Number.isInteger(Math.sqrt(person))) {
-            powerResult++;
-        }
+        
         if (powerResult > limit) {
             answer += power;
         } else {
