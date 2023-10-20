8f61893fc08db57e38dfabdfef0b662ffd41a035
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-10-20 16:19:46 +0900

    [level 1] Title: 소수 찾기, Time: 85.86 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,12 +1,12 @@
 function solution(n) {
-    const numberMap = new Map();
+    const numberMap = {};
     let count = n - 1;
     
     for (let divisor = 2; divisor <= Math.sqrt(n); divisor++) {
-        if (numberMap.has(divisor)) continue;
+        if (numberMap[divisor]) continue;
         for (let i = divisor * 2; i <= n; i += divisor) {
-            if (numberMap.has(i) === false) {
-                numberMap.set(i, true);
+            if (!numberMap[i]) {
+                numberMap[i] = true;
                 count--;
             }
         }
