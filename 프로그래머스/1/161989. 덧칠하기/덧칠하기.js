627f817119e4cf6f7d8dae4a91cdd22e48128fc9
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-10-24 18:02:20 +0900

    [level 1] Title: 덧칠하기, Time: 3.55 ms, Memory: 39.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,15 +1,25 @@
 function solution(n, m, section) {
-    const paintedSection = {};
-    let countPainting = 0;
+//     const paintedSection = {};
+//     let countPainting = 0;
     
-    for (const sectionNumber of section) {
-        if (!paintedSection[sectionNumber]) {
-            countPainting++;
-            for (let i = 0; i < m; i++) {
-                paintedSection[sectionNumber + i] = true;
-            }
+//     for (const sectionNumber of section) {
+//         if (!paintedSection[sectionNumber]) {
+//             countPainting++;
+//             for (let i = 0; i < m; i++) {
+//                 paintedSection[sectionNumber + i] = true;
+//             }
+//         }
+//     }
+//    return countPainting;
+    
+    var answer = 0;
+    var painted = 0;
+    for (var sectionNumber of section) {
+        if (painted < sectionNumber) {
+            answer++;
+            painted = sectionNumber + m - 1; // 현재 칠해져 있는 마지막 섹션 값
         }
     }
     
-    return countPainting;
+    return answer;
 }
