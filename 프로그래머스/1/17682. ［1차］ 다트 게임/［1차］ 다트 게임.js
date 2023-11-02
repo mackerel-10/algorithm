07a6f974cf757bc5a60a3ca53d0e21e3cbd06bc1
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-11-02 15:47:16 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -1,33 +1,28 @@
 function solution(dartResult) {
-    var answer = 0;
-    let i = 0, j = 0;
     const powMap = { S: 1, D: 2, T: 3 };
     const array = [];
+    let answer = 0;
+    let i = 0, j = 0;
     
     while (i < dartResult.length) {
-        if (dartResult[i] + dartResult[i + 1] === "10") {
-            console.log(dartResult[i]);
-            array[j] = Math.pow(dartResult[i] + dartResult[i + 1], powMap[dartResult[i + 2]]);
+        if (dartResult[i] + dartResult[i + 1] === "10") { // 10
+            array[j++] = Math.pow(dartResult[i] + dartResult[i + 1],
+                                powMap[dartResult[i + 2]]);
+            i += 3;
+        } else if ("0" <= dartResult[i] && dartResult[i] <= "9") { // 0 ~ 9
+            array[j++] = Math.pow(dartResult[i], powMap[dartResult[i + 1]]);
             i += 2;
-            j++;
-        } else if ("0" <= dartResult[i] && dartResult[i] <= "9") { // 점수 계산
-            console.log(dartResult[i]);
-            array[j] = Math.pow(dartResult[i], powMap[dartResult[i + 1]]);
-            i++;
-            j++;
-            console.log(array);
         } else if (dartResult[i] === "*") {
-            array[j - 1] *= 2;
-            if (j - 2 >= 0) {
+            array[j - 1] *= 2; // 현재 점수 * 2
+            if (array[j - 2] !== undefined) { // 점수가 2개 이상 존재할 때
                 array[j - 2] *= 2;
             }
+            i++;
         } else if (dartResult[i] === "#") {
             array[j - 1] *= -1;
+            i++;
         }
-        i++;
     }
-    
-    console.log(array);
     
     for (const amount of array) {
         answer += amount;
