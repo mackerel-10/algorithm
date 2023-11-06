024362b74810a10c8ba8b1b4ac1cf727c9b26247
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-11-06 14:15:32 +0900

    [level 1] Title: 옹알이 (2), Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,31 @@
+function solution(babbling) {
+    let answer = 0;
+    let wordList = {"aya": true, "ye": true, "woo": true, "ma": true};
+    
+    for (const word of babbling) {
+        let temp = '';
+        let correctLength = 0;
+        let history = [];
+        
+        if (word.length === 1) {
+            continue;
+        }
+        
+        for (let index = 0; index < word.length; index++) {
+            temp += word[index]; // 한글자씩 temp에 추가
+            
+            if (wordList[temp] === true && history[history.length - 1] !== temp) {
+                correctLength += temp.length;
+                history.push(temp);
+                temp = ''; // temp 초기화
+            }
+        }
+        
+        if (correctLength === word.length) {
+            // console.log('word', word);
+            answer++;
+        }
+    }
+    
+    return answer;
+}
