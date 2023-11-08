6f99a419d04ac4ec031f4205822708e4c0dceb34
Sujin Jeon <67633810+mackerel-10@users.noreply.github.com>
2023-11-08 18:49:37 +0900

    [level 2] Title: 전화번호 목록, Time: 100.74 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 104.22 ms
+메모리: 76.8 MB, 시간: 100.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 3일 18:43:16
+2023년 11월 3일 18:49:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,37 +1,13 @@
-// function solution(phone_book) {
-//     // 전화번호 길이를 오름차순으로 정렬
-//     phone_book.sort((phoneNum1, phoneNum2) => phoneNum1.length - phoneNum2.length);
-//     let i = 0; j = i + 1;
-    
-//     while (i < phone_book.length - 1) {
-//         if (phone_book[i][0] === phone_book[j][0]) {
-//             const prefix = phone_book[j].slice(0, phone_book[i].length);
-//             // console.log(prefix);
-//             if (phone_book[i] === prefix) return false;
-//         }
-//         j++;
-        
-//         if (j === phone_book.length) {
-//             i++;
-//             j = i + 1;
-//         }
-//     }
-    
-//     return true;
-// }
-
 function solution(phone_book) {
-    let answer = true;
-    let arr = [...phone_book];
-
-    arr = arr.sort();
-
-    for(let i = 0; i < arr.length - 1; i++) {
-        let nextString = arr[i+1].substr(0, arr[i].length);
-        if (arr[i] === nextString){
-            return false;
-        }
+    // 전화번호 길이를 오름차순으로 정렬
+    phone_book.sort();
+    // console.log(phone_book);
+    
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        let prefix = phone_book[i + 1].slice(0, phone_book[i].length);
+    
+        if (phone_book[i] === prefix) return false;
     }
-
-    return answer;
+    
+    return true;
 }
